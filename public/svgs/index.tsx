e76dcb4f18e1d8b0a6e8f5bbb3003acb44b48ee5
leejin_rho <tbdpapdl@gmail.com>
2023-11-08 18:04:33 +0900

    init: initial setting

---
+++ public/svgs/index.tsx
@@ -0,0 +1,4 @@
+import next from "./next.svg";
+
+//나중을 위한 예시
+export { next };
---
+++ src/app/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <nav className="flex space-x-4 mb-2">
+      <Link href="/" className="bg-blue-200 rounded px-4 py-2">
+        Home
+      </Link>
+      <Link href="/second" className="bg-cyan-200 rounded px-4 py-2">
+        Second
+      </Link>
+    </nav>
+  );
+}
+
+export default Header;
