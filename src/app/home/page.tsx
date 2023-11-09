f11a31279987e4d5ceb20ff411a9f08f2ec41bf1
leejin_rho <tbdpapdl@gmail.com>
2023-11-10 06:03:52 +0900

    feat: random background poster 구현

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import { Previews } from "@/components/Previews";
 import React, { useEffect, useState } from "react";
 import tmdbApi, { requests } from "@/components/api"; // import tmdbApi and requests
 import styled from "styled-components";
+import { BackgroundImg } from "@/components/BackgroundImg";
 
 interface Movie {
   id: number;
@@ -14,6 +15,7 @@ function Home() {
   const [topRated, setTopRated] = useState<Movie[]>([]);
   const [popular, setPopular] = useState<Movie[]>([]);
   const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
+  const [selectMovie, setSelectMovie] = useState<Movie>();
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -40,15 +42,35 @@ function Home() {
 
   // 영화 목록을 렌더링하는 함수
   const renderMovies = (movies: Movie[]) => {
-    return movies.map((movie) => (
-      <div key={movie.id} className="flex-shrink-0">
-        <h3>{movie.title}</h3>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={`${movie.title} poster`}
+    return movies.map((movie) => {
+      return (
+        <div key={movie.id}>
+          <h3>{movie.title}</h3>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={`${movie.title} poster`}
+          />
+        </div>
+      );
+    });
+  };
+
+  const randomMovie = (movies: Movie[]) => {
+    useEffect(() => {
+      if (movies && movies.length > 0) {
+        const randomIndex = Math.floor(Math.random() * movies.length);
+        const movie = movies[randomIndex];
+        setSelectMovie(movie);
+      }
+    }, [movies]);
+
+    return selectMovie ? (
+      <div>
+        <BackgroundImg
+          imageUrl={`https://image.tmdb.org/t/p/w500${selectMovie.poster_path}`}
         />
       </div>
-    ));
+    ) : null;
   };
 
   const renderPreviews = (movies: Movie[]) => {
@@ -61,7 +83,6 @@ function Home() {
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={`${movie.title} poster`}
           /> */}
-
           <Previews imageUrl={imageUrl} />
         </div>
       );
@@ -78,6 +99,7 @@ function Home() {
 
       <h2>Now Playing</h2>
       <div>{renderMovies(nowPlaying)}</div> */}
+      <BackgroundBox>{randomMovie(topRated)}</BackgroundBox>
       <PreviewBox className="pl-[0.75rem] mt-[29.5rem] w-[100%] flex flex-col">
         <span className="pl-[0.25rem] text-white mt-[2.69rem] mb-[1.44rem] text-preview font-bold">
           Previews
@@ -97,6 +119,14 @@ export default Home;
 const Container = styled.div`
   display: flex;
   justify-content: start;
+`;
+
+const BackgroundBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  position: absolute;
 `;
 
 const PreviewBox = styled.div`
