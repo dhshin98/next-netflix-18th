db36c8ca0dca3872da1e09c99177fd7af38ac2eb
dhsin98 <shinjay417@gmail.com>
2023-11-10 19:26:05 +0900

    feat: RandomMovie 부분 변경

--- src/app/utils/movieFunctions.tsx
+++ src/app/utils/movieFunctions.tsx
@@ -1,6 +1,5 @@
-import { SquareImg, Previews } from "@/components/ImageType";
-import { BackgroundImg } from "@/components/BackgroundImg";
-import { useEffect, useState } from "react";
+import { SquareImg, Previews, BackgroundImg } from "@/components/ImageType";
+import { useEffect, useState, useMemo } from "react";
 import { SquareForRankingIcon, Top10Icon } from "../../../public/svgs";
 
 interface Movie {
@@ -11,29 +10,36 @@ interface Movie {
 
 //페이지의 가장 위쪽에 랜덤으로 함수를 보여주는 함수
 export const RandomMovie = ({ movies }: { movies: Movie[] }) => {
-  const [selectMovie, setSelectMovie] = useState<Movie | undefined>();
-  const [idx, setIdx] = useState<number | undefined>();
+  const [selectedData, setSelectedData] = useState<
+    { movie: Movie; index: number } | undefined
+  >();
+
   useEffect(() => {
     if (movies.length > 0) {
       //0-9위 사이의 영화만 추려내기
-      const maxIndex = Math.min(movies.length, 10);
-      const randomIndex = Math.floor(Math.random() * maxIndex);
-      const movie = movies[randomIndex];
-      setSelectMovie(movie);
-      setIdx(randomIndex);
+      setSelectedData(() => {
+        const maxIndex = Math.min(movies.length, 10);
+        const randomIndex = Math.floor(Math.random() * maxIndex);
+        return { movie: movies[randomIndex], index: randomIndex };
+      });
     }
   }, [movies]);
 
-  return selectMovie ? (
+  // useMemo를 통해 selectedData의 값이 변경될 때만 새로 계산
+  const { movie, index } = useMemo(() => {
+    return selectedData || { movie: undefined, index: undefined };
+  }, [selectedData]);
+
+  return movie ? (
     <div className="flex flex-col items-center justify-center w-[100%]">
-      <BackgroundImg imageUrl={getImageUrl(selectMovie.poster_path)} />
+      <BackgroundImg imageUrl={getImageUrl(movie.poster_path)} />
       <span className="flex align-center mt-2 md-2">
         <div className="relative mr-2">
           <SquareForRankingIcon />
           <Top10Icon className="absolute inset-0 m-auto" />
         </div>
         <div className=" text-xs font-bold leading-tight text-white md-2">
-          #{idx} in Korea Today
+          #{index} in Korea Today
         </div>
       </span>
     </div>
--- src/components/ImageType.tsx
+++ src/components/ImageType.tsx
@@ -1,20 +1,40 @@
-"use client";
 import Image from "next/image";
-export const SquareImg = ({ imageUrl }: { imageUrl: string }) => {
+import styled from "styled-components";
+
+//home 화면 가장 위에 보이는 image
+export const BackgroundImg = ({ imageUrl }: { imageUrl: string }) => {
   return (
-    <div
-      style={{ width: "6.4375rem", height: "10.0625rem", position: "relative" }}
-    >
+    <ImageWrapper>
       <Image
         src={imageUrl}
-        alt="popularimage"
+        alt={"backgroundImg"}
         layout="fill"
         objectFit="cover"
       />
-    </div>
+    </ImageWrapper>
   );
 };
 
+const ImageWrapper = styled.div`
+  display: flex;
+  position: relative;
+  overflow: hidden;
+  width: 375px;
+  height: 415px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40%;
+    background: linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0));
+    z-index: 2;
+  }
+`;
+
+//홈화면 중간에 동그라미로 보이는 image
 export const Previews = ({ imageUrl }: { imageUrl: string }) => {
   return (
     <div
@@ -26,6 +46,22 @@ export const Previews = ({ imageUrl }: { imageUrl: string }) => {
         layout="fill"
         objectFit="cover"
         className="rounded-full"
+      />
+    </div>
+  );
+};
+
+//홈화면 아래쪽에 보니는 image
+export const SquareImg = ({ imageUrl }: { imageUrl: string }) => {
+  return (
+    <div
+      style={{ width: "6.4375rem", height: "10.0625rem", position: "relative" }}
+    >
+      <Image
+        src={imageUrl}
+        alt="popularimage"
+        layout="fill"
+        objectFit="cover"
       />
     </div>
   );
